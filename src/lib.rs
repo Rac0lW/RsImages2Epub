22fd3bf350a12e6eb0fb661561168b58adb29f96
Revo Wu <rev027w@gmail.com>
2025-02-04 18:59:58 +0800

    Update:
    Optimized some of the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use directories::UserDirs;
-
+use epub_builder::{EpubBuilder, EpubContent, EpubVersion, ReferenceType, ZipLibrary};
+use std::fs;
 pub fn get_desktop_path() -> String {
     if let Some(user_dir) = UserDirs::new() {
         if let Some(path) = user_dir.desktop_dir() {
@@ -12,4 +13,57 @@ pub fn get_desktop_path() -> String {
     } else {
         panic!("Could not determine user directories.");
     }
+}
+
+pub fn run(image_folder: &str) -> epub_builder::Result<Vec<u8>> {
+    let mut output = Vec::<u8>::new();
+
+    let zip_library = ZipLibrary::new()?;
+
+    let mut builder = EpubBuilder::new(zip_library).unwrap();
+
+    builder.metadata("author", "unknown")?;
+    builder.metadata("title", "unknown")?;
+    builder.epub_version(EpubVersion::V30);
+
+    // Read images from the folder and add them to the EPUB
+    let paths = fs::read_dir(image_folder)?;
+
+    let mut cover_index_flag: i32 = 1;
+
+    for (i, path) in paths.enumerate() {
+        let path = path?.path();
+        if path.is_file() {
+            let image_data = fs::read(&path)?;
+            let image_name = path.file_name().unwrap().to_str().unwrap();
+            if cover_index_flag == 1 {
+                builder.add_cover_image(image_name, &*image_data, "image/png")?;
+                cover_index_flag += 1;
+            }
+            // Add image as a resource
+            builder.add_resource(image_name, &*image_data, "image/png")?;
+
+            // Create XHTML content for each image
+            let xhtml_content = format!(
+                r#"<?xml version="1.0" encoding="UTF-8"?>
+                <html xmlns="http://www.w3.org/1999/xhtml">
+                <head><title>Image {}</title></head>
+                <body><img src="{}" alt="Image {}" /></body>
+                </html>"#,
+                i + 1,
+                image_name,
+                i + 1
+            );
+
+            builder.add_content(
+                EpubContent::new(format!("image_{}.xhtml", i + 1), xhtml_content.as_bytes())
+                    .title(format!("Image {}", i + 1))
+                    .reftype(ReferenceType::Text),
+            )?;
+        }
+    }
+
+    builder.generate(&mut output)?;
+
+    Ok(output)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,27 @@
 
-use epub_builder::{EpubBuilder, EpubContent, EpubVersion, ReferenceType, ZipLibrary};
+
 use std::error::Error;
-use std::fs::{self, File};
-use std::io::{self, Write};
-// use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 use ansi_term::Colour;
-use rs_image_2_epub::get_desktop_path;
-
-fn run(image_folder: &str) -> epub_builder::Result<Vec<u8>> {
-    let mut output = Vec::<u8>::new();
-
-    let zip_library = ZipLibrary::new()?;
-
-    let mut builder = EpubBuilder::new(zip_library).unwrap();
-
-    builder.metadata("author", "unknown")?;
-    builder.metadata("title", "unknown")?;
-    builder.epub_version(EpubVersion::V30);
-
-    // Read images from the folder and add them to the EPUB
-    let paths = fs::read_dir(image_folder)?;
-
-    let mut cover_index_flag: i32 = 1;
-
-    for (i, path) in paths.enumerate() {
-        let path = path?.path();
-        if path.is_file() {
-            let image_data = fs::read(&path)?;
-            let image_name = path.file_name().unwrap().to_str().unwrap();
-            if cover_index_flag == 1 {
-                builder.add_cover_image(image_name, &*image_data, "image/png")?;
-                cover_index_flag += 1;
-            }
-            // Add image as a resource
-            builder.add_resource(image_name, &*image_data, "image/png")?;
-
-            // Create XHTML content for each image
-            let xhtml_content = format!(
-                r#"<?xml version="1.0" encoding="UTF-8"?>
-                <html xmlns="http://www.w3.org/1999/xhtml">
-                <head><title>Image {}</title></head>
-                <body><img src="{}" alt="Image {}" /></body>
-                </html>"#,
-                i + 1,
-                image_name,
-                i + 1
-            );
-
-            builder.add_content(
-                EpubContent::new(format!("image_{}.xhtml", i + 1), xhtml_content.as_bytes())
-                    .title(format!("Image {}", i + 1))
-                    .reftype(ReferenceType::Text),
-            )?;
-        }
-    }
-
-    builder.generate(&mut output)?;
-
-    Ok(output)
-}
+use rs_image_2_epub::{get_desktop_path, run};
+use log::info;
 
 use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct args{
+struct Args{
     #[arg(short, long)]
     address: String
 }
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    info!("start up");
     // Path to the folder containing images
-    let args = args::parse();
+    let args = Args::parse();
 
     let input = args.address;
 
@@ -96,4 +46,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     Ok(())
+
+    
 }
